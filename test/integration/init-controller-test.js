b841db859530df8d26a5f21e4dd987a4ac5df4cf
Gordon <gordonlarrigan@gmail.com>
2021-03-20 20:59:10 +0000

    Improve init controller test

--- test/integration/init-controller-test.js
+++ test/integration/init-controller-test.js
@@ -2,6 +2,19 @@ const mockArgv = require('mock-argv');
 const mockRequire = require('mock-require');
 const CaptureStdOut = require('capture-stdout');
 
+// Mock kanbn library
+const mockKanbn = {
+  async initialised() {
+    return false;
+  },
+  getMainFolder() {
+    return 'test';
+  },
+  async initialise(options = {}) {
+    console.log(JSON.stringify(options));
+  }
+};
+
 QUnit.module('init controller tests', {
   before() {
     require('../qunit-contains');
@@ -11,17 +24,7 @@ QUnit.module('init controller tests', {
 QUnit.test('Initialise kanbn with no options', async assert => {
 
   // Mock kanbn library
-  mockRequire('../../src/main', {
-    async initialised() {
-      return false;
-    },
-    getMainFolder() {
-      return 'test';
-    },
-    async initialise(options = {}) {
-      console.log(options);
-    }
-  });
+  mockRequire('../../src/main', mockKanbn);
   const index = require('../../index');
 
   // Start capturing output
@@ -29,12 +32,42 @@ QUnit.test('Initialise kanbn with no options', async assert => {
   output.startCapture();
 
   // Call initialise command
-  await mockArgv(['init', '-n', 'test123'], index);
+  await mockArgv(['init'], index);
 
   // Check output
   output.stopCapture();
-  assert.contains(output.getCapturedText(), "{ name: 'test123' }");
+  assert.deepEqual(
+    JSON.parse(output.getCapturedText()[0]),
+    {}
+  );
   assert.contains(output.getCapturedText(), 'Initialised empty kanbn board in test');
 });
 
-// TODO finish integration tests
+QUnit.test('Initialise kanbn with all options', async assert => {
+
+  // Mock kanbn library
+  mockRequire('../../src/main', mockKanbn);
+  const index = require('../../index');
+
+  // Start capturing output
+  const output = new CaptureStdOut();
+  output.startCapture();
+
+  // Call initialise command
+  await mockArgv(['init', '-n', 'test123', '-d', 'Test description', '-c', 'Column 1', '-c', 'Column 2'], index);
+
+  // Check output
+  output.stopCapture();
+  assert.deepEqual(
+    JSON.parse(output.getCapturedText()[0]),
+    {
+      name: 'test123',
+      description: 'Test description',
+      columns: [
+        'Column 1',
+        'Column 2'
+      ]
+    }
+  );
+  assert.contains(output.getCapturedText(), 'Initialised empty kanbn board in test');
+});
